4e3ed3606a50cf7ff11529271a7eeb4dad480f29
Redxjak <derrickhazlett@gmail.com>
2023-07-14 22:30:55 -0500

    Updates

---
+++ precache-manifest.56b966555e6b22bc89a385ec93957604.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376d64b2086c6cad47c5b581fc0d7c5f",
+    "url": "/ArmyMemoMaker/index.html"
+  },
+  {
+    "revision": "ec39a9d8361b43153285",
+    "url": "/ArmyMemoMaker/static/css/main.3833ceb4.chunk.css"
+  },
+  {
+    "revision": "a317cef9a929bace39e5",
+    "url": "/ArmyMemoMaker/static/js/2.5fb4ec1e.chunk.js"
+  },
+  {
+    "revision": "3c0593b56b59120cbbfa8405938d8538",
+    "url": "/ArmyMemoMaker/static/js/2.5fb4ec1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec39a9d8361b43153285",
+    "url": "/ArmyMemoMaker/static/js/main.420890a4.chunk.js"
+  },
+  {
+    "revision": "3ea836e34286c8e586e6",
+    "url": "/ArmyMemoMaker/static/js/runtime-main.26b4941a.js"
+  },
+  {
+    "revision": "c4c16d8a7226dadca83937346bdfa0e5",
+    "url": "/ArmyMemoMaker/static/media/DoD Seal.c4c16d8a.PNG"
+  },
+  {
+    "revision": "c4c16d8a7226dadca83937346bdfa0e5",
+    "url": "/ArmyMemoMaker/static/media/attnFromExample.c4c16d8a.JPG"
+  }
+]);
